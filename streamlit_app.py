7bd1cf1092504c6e7be37c3f07a7df9d704e7b5f
mmohammd98 <mmohammad2508@gmail.com>
2025-03-27 12:00:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Hello, Streamlit!")
+st.write("This is a simple Streamlit app.")
+
+name = st.text_input("Enter your name")
+age = st.slider("Select your age", 0, 100)
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.write(data)
+    
+    
+
+if st.button("Click Me"):
+    st.write(f"Hello, {name}! You are {age} years old.")
+    
+
+option = st.selectbox(
+    'Choose an option',
+    ['Option 1', 'Option 2', 'Option 3']
 )
+st.write('You selected:', option)
